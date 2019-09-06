297f2d4b1735b4c9b51705f7f84a507ef3805a57
jonnalagadda35153 <38223152+jonnalagadda35153@users.noreply.github.com>
2019-09-06 15:37:24 -0400

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World from tenantone";
+        return "Hello Docker World from Tenantg One..!!";
     }
 
     public static void main(String[] args) {
